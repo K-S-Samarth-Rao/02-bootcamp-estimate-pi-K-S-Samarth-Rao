c175cb282022b307d1533f2720e90650bf8cf5a3
K S Samarth Rao <renukarao76@gmail.com>
2021-05-31 21:15:33 +0530

    Final Program

--- estimate.py
+++ estimate.py
@@ -1,12 +1,35 @@
 import math
+import random
 import unittest
+
+def wallis(n):                                   # Function to use wallis formula
+    product = 1.0 
+    for i in range(1,n+1):
+        numb = (4*(i**2))/((4*(i**2))-1)
+        product*=numb                
+    print("Value of pi =",product*2)             # Here product will be pi/2
+    
+def monte_carlo(n):
+    Points_Circle = 0                            # No. of points inside Circle
+    Points_Square = 0                            # No. of points inside Square
+    for i in range(1,n+1):
+        x = random.random()
+        y = random.random()
+         
+        Dist = x**2 + y**2                       # Distance of point (x,y) from origin (0,0)  
+        if ( Dist<=1 ):
+           Points_Circle+=1
+          
+        Points_Square+=1
+    pi = 4*Points_Circle/Points_Square           # Estimating the value of pi
+    print(" Estimated Value of Pi =",pi)            
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
+
     def test_high_iters(self):
         for i in range(500, 600):
             pi = wallis(i)
@@ -17,7 +40,7 @@ class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
+
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
@@ -26,7 +49,9 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+
+
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()        
+       
+
